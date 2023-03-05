7a727e9ec2fb61e81a73ccd3e544d722aa875d1f
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2023-03-05 01:57:27 -0500

    My server has wings now

--- zaman/server.py
+++ zaman/server.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
+import json
 
 app = Flask(__name__)
 
@@ -14,9 +15,21 @@ def all_interests(user_name):
 def user_bundle(user_name,interest):
     return f'{user_name}: {interest}'
 
-@app.route('/ext/<user_name>/<interest>')
-def user_bundle_data(user_name,interest):
-    return f'raw data for: {user_name}: {interest}'
+@app.route('/ext', methods=['GET', 'OPTIONS'])
+def user_bundle_data():
+
+    messages = {}
+
+    if request.method == 'GET':
+        messages["data"] = "Hello world"
+
+    resp = make_response(jsonify(messages), 200)
+
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
+    resp.headers['Access-Control-Allow-Headers'] = 'Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization'
+    
+    return resp
 
 if __name__ == '__main__':
     app.run(debug=True)
