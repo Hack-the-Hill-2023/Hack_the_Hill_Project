20b010b4c681caa8f7f733b136b31488012a6b63
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2023-03-05 01:57:38 -0500

    You don't mind if i park this here?

--- extenstion/content-script.js
+++ extenstion/content-script.js
@@ -1,4 +1,29 @@
+let start = document.getElementsByClassName('richtext')[0];
 let div = document.createElement("div"); 
-document.body.appendChild(div); 
-div.innerText="test123";
-console.log("Hello world")
+div.classList.add("aem-GridColumn--default--12")
+div.classList.add("aem-GridColumn")
+
+start.parentNode.insertBefore(div, start.nextSibling);
+
+fetch("http://127.0.0.1:5000/ext", {
+    method: "GET",
+})
+.then((res) => {
+    if (res.ok) { // ok if status is 2xx
+        console.log(res.status);
+    } else {
+        console.log('Request failed.  Returned status of ' + res.status);
+    }
+
+    return res.blob()
+})
+.then((blob) => {
+    blob.text()
+    .then((data) => JSON.parse(data))
+    .then((text) => {
+
+        div.innerText=text.data
+    })
+    
+    // window.result = blob
+})
