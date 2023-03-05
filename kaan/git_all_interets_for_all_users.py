7bc72b7a552dc5dca51c7de31a12c431e46ce0bd
unKaan <84788143+unKaan@users.noreply.github.com>
2023-03-05 05:34:06 -0500

    Updated the original file

--- kaan/git_all_interets_for_all_users.py
+++ kaan/git_all_interets_for_all_users.py
@@ -6,20 +6,19 @@ filename = './generated.json'
 def parser():
     fcc_data = None
     array= []
-    array2 = []
-    array3 = []
+
     with open(filename, 'r') as fcc_file:
         fcc_data = json.load(fcc_file)
 
     for i in fcc_data:
+        array2 = []
         array2.append(i["index"])
         array2.append(i["name"])
-
+        array3 = []
         for intgr in range(3):
             array3.append(i["purchase"][intgr]["product_category"])
-        array3 = []
         array2.append(array3)
-    array.append(array2)
+        array.append(array2)
 
     return array
 
