5cb41e789f83c1f64a32579283dee96d229cd23a
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2023-03-04 22:27:57 -0500

    created routing for each page

--- zaman/server.py
+++ zaman/server.py
@@ -1,10 +1,22 @@
-rom flask import Flask
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hello/<name>')
-def hello(name):
-    return f'Hello, {name}!'
+@app.route('/devtool')
+def all_users():
+    return f'dev tools'
+
+@app.route('/devtool/<user_name>')
+def all_interests(user_name):
+    return f'{user_name}`s interests'
+
+@app.route('/devtool/<user_name>/<interest>')
+def user_bundle(user_name,interest):
+    return f'{user_name}: {interest}'
+
+@app.route('/ext/<user_name>/<interest>')
+def user_bundle_data(user_name,interest):
+    return f'raw data for: {user_name}: {interest}'
 
 if __name__ == '__main__':
     app.run(debug=True)
