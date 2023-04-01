545ac622530fefebbd0598c5e23b74e753812ac1
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2023-03-31 20:30:47 -0400

    Added documentation

--- zaman/Json_Parser.py
+++ zaman/Json_Parser.py
@@ -1,22 +1,26 @@
 import random
 import json
 
+# The file path for the JSON file to be parsed
 filename = 'zaman/generated.json'
 
-
 def parser(name):
+    # Initialize the fcc_data variable
     fcc_data = None
     
+    # Open the JSON file for reading and load its contents into fcc_data
     with open(filename, 'r') as fcc_file:
         fcc_data = json.load(fcc_file)
-    # print(type(fcc_data))
-    # print(fcc_data)
+    
+    # Iterate through the fcc_data JSON object and return a randomly chosen product category for the given name
     for i in fcc_data:
         if name == i["name"]:
             rand=random.randint(0,len(i["purchase"])-1)
             return i["purchase"][rand]["product_category"]
 
+    # Return an empty string if the name is not found in the JSON file
     return ""
 
+# Test the parser function with a specific name
 if __name__ == '__main__':
     print(parser("Lucile Roman"))
