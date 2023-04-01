e2efa0381d03984e716df7b68778e27c4fdea4fd
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2023-03-31 20:03:31 -0400

    Added documentation

--- extenstion/content-script.js
+++ extenstion/content-script.js
@@ -1,37 +1,26 @@
+// Select the first element with the class 'richtext'
 let start = document.getElementsByClassName('richtext')[0];
+
+// Create a new div element
 let div = document.createElement("div"); 
+
+// Add two classes to the new div element
 div.classList.add("aem-GridColumn--default--12")
 div.classList.add("aem-GridColumn")
 
+// Insert the new div element before the element selected in the first step
 start.parentNode.insertBefore(div, start.nextSibling);
 
+// Create a new iframe element
 let iframe = document.createElement("iframe")
 
+// Set the CSS styles for the iframe element
 iframe.style.border = "none"
 iframe.style.width = "100%"
 iframe.style.minHeight = "300px"
 
+// Set the source attribute of the iframe element
 iframe.setAttribute("src", "http://127.0.0.1:5000/ext");
 
+// Append the iframe element to the newly created div element
 div.appendChild(iframe)
-
-// fetch("http://127.0.0.1:5000/ext", {
-//     method: "GET",
-// })
-// .then((res) => {
-//     if (res.ok) { // ok if status is 2xx
-//         console.log(res.status);
-//     } else {
-//         console.log('Request failed.  Returned status of ' + res.status);
-//     }
-
-//     return res.blob()
-// })
-// .then((blob) => {
-//     blob.text()
-//     .then((data) => JSON.parse(data))
-//     .then((text) => {
-
-//         div.innerHTML=text.data
-//     })
-// })
