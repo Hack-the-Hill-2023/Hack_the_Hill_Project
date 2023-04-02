41b827ad1f393c5a8cb86ddf51fe80b2e831e21f
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2023-04-01 23:43:21 -0400

    Added documentation

--- zaman/product_parser.py
+++ zaman/product_parser.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+# constant used to spoof HTTP request inorder get valid json data from Canadian Tire Search API
 HEADERS = (
     {
         'accept': 'application/json, text/plain, */*',
@@ -27,7 +28,17 @@ HEADERS = (
 )
 
 class Product:
+    """
+    Product class stores state and method related with a Canadian Tire Product
+    """
     def __init__(self, name, rating, price, image, url):
+        """
+        :param name: Canadian tire product name
+        :param rating: Rating of product
+        :param price: intial price of product
+        :param image: url of product image
+        :param url: url of canadian tire product
+        """
         self.name = name
         self.rating = rating
         self.price = price
@@ -41,16 +52,38 @@ class Product:
     #     return f"{self.name[:10]}..." if len(self.name) > 10 else self.name
     
 def get_url(topic: str, page: int, count: int = 10) -> str:
+    """
+    get_url function creates the required url inorder to search using the Canadian Tire Serach API
+    
+    :param topic: String of topic that is being searched
+    :param page: I don't know what this does after a couple minutes of testing
+    :param count: Max number of products that can be returned in a search
+    """
     return "https://apim.canadiantire.ca/v1/search/search?q={0};page={1};store=659;lang=en_CA;count={2}".format(topic, page, count)
 
 def get_data(url: str):
+    """
+    get_data returns data from request
+    
+    :param url:
+    """
     r = requests.get(url, headers=HEADERS)
     return r.text
 
 def parse_json(raw: str):
+    """
+    parse_json returns json of data
+    
+    :param raw: unparsed response http body
+    """
     return json.loads(raw)
 
 def extract_products(products) -> [Product]:
+    """
+    extract_products function converts a json object into a list of Products 
+    
+    :param products: Json repersetnation of a product
+    """
     data = []
 
     if products != None:
@@ -68,6 +101,11 @@ def extract_products(products) -> [Product]:
     return data
 
 def get_products(item: str) -> [Product]:
+    """
+    get_products function gets a list of products from an item prompt
+    
+    :param item: iitial prompt used to get a list of products
+    """
     url = get_url(item, 1)
 
     raw_data = get_data(url)
